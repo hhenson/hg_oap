6cb4f56960a4e3b5346a393e0a49faa0aadb2fd1
Simon Young <168648279+syoung-bamfunds@users.noreply.github.com>
2024-08-23 16:05:56 +0100

    Delete tests/unit/hg_oap/instruments/test_fx_spot.py
    Added in error

--- tests/unit/hg_oap/instruments/test_fx_spot.py
+++
@@ -1,6 +0,0 @@
-from mzar.domain.instruments.fx import FXSpot
-from mzar.refdata.universe.currencies import Currencies
-
-
-def test_fx_spot_symbol():
-    assert FXSpot(base=Currencies.GBP.value, quote=Currencies.JPY.value).symbol == 'GBPJPY.SPOT'
